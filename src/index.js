ddcbdbbe98a518594c7de2cc6b9f8c0e8ec0797a
yoonisali <yoonismahamoudali@gmail.com>
2022-11-15 16:17:39 -0800

    add trending button
    co-authored-by: Chris Loveless <chriskloveless@gmail.com>

--- src/index.js
+++ src/index.js
@@ -10,24 +10,50 @@ function getGif(keyword) {
     const response = JSON.parse(this.responseText);
     let array = response.data;
     if (this.status === 200) {
-      displayContent(response, keyword, array);
+      displayContent(keyword, array);
     }
-
   });
   request.open("GET", url, true);
   request.send();
 }
 
+const getTrend = () => {
+  let request = new XMLHttpRequest();
+  const url = `https://api.giphy.com/v1/gifs/trending?api_key=${process.env.API_KEY}&limit=10&rating=g`;
+
+  request.addEventListener("loadend", function () {
+    const response = JSON.parse(this.responseText);
+    let array = response.data;
+    if (this.status === 200) {
+      displayTrend(array);
+    }
+  });
+  request.open("GET", url, true);
+  request.send();
+};
+
 
 // UI Logic---------------------------------------------
-function displayContent(apiResponse, keyword, array) {
-
+function displayContent(keyword, array) {
+console.log(keyword);
   document.getElementById("output").innerText = `Your search results for ${keyword} are ...`;
 
   array.forEach(function (element) {
-    let imgElement = document.createElement("img");imgElement.setAttribute("src", element.images.fixed_width.url);
+    let imgElement = document.createElement("img");
+    imgElement.setAttribute("src", element.images.fixed_width.url);
     document.getElementById("output").append(imgElement);
   }); 
+}
+
+function displayTrend(array) {
+
+  document.getElementById("output").innerText = `Your trending GIF's are ...`;
+
+  array.forEach(function (element) {
+    let imgElement = document.createElement("img");
+    imgElement.setAttribute("src", element.images.fixed_width.url);
+    document.getElementById("output").append(imgElement);
+  });
 }
 
 function handleFormSubmission(event) {
@@ -38,6 +64,13 @@ function handleFormSubmission(event) {
   getGif(keyword);
 }
 
+function handleTrend(event) {
+  event.preventDefault();
+  getTrend();
+}
+
 window.addEventListener("load", function () {
   document.getElementById('form').addEventListener("submit", handleFormSubmission);
+  document.getElementById("trend-btn").addEventListener("click", handleTrend);
+
 });
